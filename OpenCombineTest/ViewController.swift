ce6d557bae640bd37fd050dd4873c6cb36277cac
hyungsung-kim <hyungsung.kim@navercorp.com>
2019-11-20 21:02:42 +0900

    Initial Commit for OpenCombine crash test

--- OpenCombineTest/ViewController.swift
+++ OpenCombineTest/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// It not make a crash at line 26
+//import Combine
+// But this is making a crash at line 26 in XCode 11.2.1
+import OpenCombine
 
+class ViewController: UIViewController {
+    
+    let testSubject = CurrentValueSubject<Int, Never>(5)
+    var testInt: Int = 0
+    
+    private var testSubscription: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // It must make a crash.
+        // Crash happened at 'Subscribers.Sink.swift, line: 56, subscription.request(.unlimited), Error: EXC_BAD_ACCESS (code=2, address=0x10d96e889)
+        self.testSubscription = self.testSubject.sink(receiveValue: { (value) in
+            print("value received = \(value)")
+        })
     }
 
-
+    @IBAction
+    func touchUpInsideOfTestButton(_ button: UIButton) {
+        self.testInt += 1
+        self.testSubject.value = self.testInt
+    }
 }
 
